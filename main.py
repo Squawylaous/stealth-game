24104276bc83107c18061d7658eb90cbc877f86e
Squawylaous <57704977+Squawylaous@users.noreply.github.com>
2022-04-14 12:02:39 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,60 @@
+import operator as op
+from functools import partial
+from itertools import chain
+import pygame
+from pygame.locals import *
+from pygame.math import Vector2 as vector
+
+pygame.init()
+
+clock = pygame.time.Clock()
+font = pygame.font.SysFont("Arial", 25)
+background = Color(161, 161, 161)
+foreground = Color(255, 255, 255)
+screen = pygame.display.set_mode((0, 0), FULLSCREEN)
+screen_rect = screen.get_rect()
+update_rects = [[]]
+fps = 0
+
+group_dict = lambda d:{i:v for k, v in d.items() for i in k}
+
+directions = group_dict({(K_w, K_UP, 1):(0, -1), (K_s, K_DOWN, 2):(0, 1), (K_a, K_LEFT, 4):(-1 ,0), (K_d, K_RIGHT, 8):(1, 0)})
+
+def intVector(v):
+  return (*map(int, map(round, v)),)
+
+
+class Player:
+  def __init__(self, pos, angle):
+    self.pos, self.angle = vector(pos), vector(1, 0).rotate(angle)
+    self.speed = 0
+    self.top_speed = 12.5
+  
+  @property
+  def velocity(self):
+    return self.angle * self.speed
+
+screen.fill(background)
+pygame.display.flip()
+
+player = Player((0, 0), -90)
+
+while True:
+  clock.tick(60)
+  fps = clock.get_fps()
+  update_rects = [update_rects[1:]]
+  screen.fill(background)
+  
+  if pygame.event.get(QUIT):
+    break
+  for event in pygame.event.get():
+    if event.type == KEYDOWN:
+      if event.key == K_ESCAPE:
+        pygame.event.post(pygame.event.Event(QUIT))
+      if event.key in directions:
+        player.input(event.key)
+  
+  screen.blit(font.render(str(int(fps)), 0, foreground), (0,0))
+  update_rects.append(pygame.rect.Rect((0,0), font.size(str(int(fps)))))
+  pygame.display.update(update_rects[0]+update_rects[1:])
+pygame.quit()
